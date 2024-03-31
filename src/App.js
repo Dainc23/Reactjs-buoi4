1d310843225864ac25c27f035f4f299aca4095ef
Dainc23 <ncd2331999@gmail.com>
2024-03-31 15:56:07 +0700

    public lan 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hook1 from './Components/Hook1';
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Hook1 />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Hook1.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
+const Hook1 = () => {
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+    return (
+        <div>
+            <Button color="danger" onClick={toggle}>
+                Click Me
+            </Button>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label
+                                for="exampleEmail"
+                                hidden
+                            >
+                                Email
+                            </Label>
+                            <Input
+                                id="exampleEmail"
+                                name="Nhap ten khach hang"
+                                placeholder="Email"
+                                type="email"
+                            />
+                        </FormGroup>
+                        {' '}
+                        <FormGroup>
+                            <Label
+                                for="examplePassword"
+                                hidden
+                            >
+                                Password
+                            </Label>
+                            <Input
+                                id="examplePassword"
+                                name="password"
+                                placeholder="Nhap mat khau"
+                                type="password"
+                            />
+                        </FormGroup>
+                        {' '}
+                        
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>
+                        Mua ngay
+                    </Button>
+                    <Button color="secondary" onClick={toggle}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    );
+}
+
+export default Hook1;
